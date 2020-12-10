a671b29d88c5486a0f43875688a17c5f74cc6e10
Khusboo Kumari <36759104+khush611@users.noreply.github.com>
2020-12-11 01:35:16 +0530

    changes

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,29 @@
-import React from 'react';
+// import React from 'react';
+
+// const ColourSelector = (props) => {
+//   const { config, selectNextBackground } = props
+//   const { background } = config;
+//   return (
+//     <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
+//       {/* label should come here */}
+//     </button>
+//   )
+// }
+// export default ColourSelector;
+
+import React from "react";
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
+  const { config, selectNextBackground } = props;
   const { background } = config;
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
+    <button
+      className={config.classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
       {/* label should come here */}
+      {config.label}
     </button>
-  )
-}
+  );
+};
 export default ColourSelector;
